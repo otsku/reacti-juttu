8eb58ec4f6e3f507490c15c7a8b58db429f26854
otsku <otto.pelit@gmail.com>
2019-03-10 19:44:47 +0200

    Updates

---
+++ precache-manifest.31e72989a47d3a70216df3ca79a197ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reacti-juttu/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7a2c40515ac1c983e977",
+    "url": "/reacti-juttu/static/js/runtime~main.76a770f8.js"
+  },
+  {
+    "revision": "d2e2c99e89c412723327",
+    "url": "/reacti-juttu/static/js/main.d15fb18a.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/reacti-juttu/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "d2e2c99e89c412723327",
+    "url": "/reacti-juttu/static/css/main.8b05262e.chunk.css"
+  },
+  {
+    "revision": "c253683f0b60536be592dcbc37359f23",
+    "url": "/reacti-juttu/index.html"
+  }
+];
