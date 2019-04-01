b0f5fde00a2a4e4ce0ff6c2175324479a4d90468
otsku <otto.pelit@gmail.com>
2019-04-01 17:18:34 +0300

    sivutus tehty

--- src/App.js
+++ src/App.js
@@ -10,9 +10,12 @@ class App extends React.Component {
 			pics: [],
 			error: null,
 			Details: false,
-			id: null
+			id: null,
+			currentPage: 1,
+          	picsPerPage: 100
 		}
 		this.handleClick = this.handleClick.bind(this);
+		this.handlePage = this.handlePage.bind(this);
 	}
 	
 	componentDidMount() {
@@ -20,6 +23,12 @@ class App extends React.Component {
 			this.fetchPics();
 		}	
 	}
+
+	handlePage(event) {
+        this.setState({
+          currentPage: Number(event.target.id)
+        });
+      }
 
 	fetchPics() {
 		// Where we're fetching data from
@@ -43,41 +52,76 @@ class App extends React.Component {
 	}
 
 	render() {
-		const { isLoading, pics, error } = this.state;
+		//const { isLoading, pics, error } = this.state;
 		const divStyle = {
 			display: 'flex',
 			flexWrap: 'wrap',
+			margin: '0',
 		};
 		const cell = {
 			display: 'flex',
 			flexWrap: 'wrap',
-			padding: '4px 1px 4px 1px',
-  			margin: 'auto',
+			padding: '4px 4px 4px 4px',
 		};
+		const pageStyle2 = {
+			flexWrap: 'wrap',
+			listStyle: 'none',
+			display: 'flex',
+			paddingRight: '4px',
+			color: 'blue',
+			userSelect: 'none',
+			cursor: 'pointer',
+			fontFamily: 'Roboto , Arial, sans-serif',
+			fontSize: '1.2em',
+		};
+		const { pics, currentPage, picsPerPage } = this.state;
+
+        // Logic for displaying current todos
+        const indexOfLastPic = currentPage * picsPerPage;
+        const indexOfFirstPic = indexOfLastPic - picsPerPage;
+        const currentPics = pics.slice(indexOfFirstPic, indexOfLastPic);
+
 		if(this.state.Details === true) {
 			return(<Details pic={this.state.id} />);
 		}
-		return (
-		 	<React.Fragment>
-				<h1>Pictures</h1>
-				<div style={divStyle}>
-				{error ? <p>{error.message}</p> : null}
-				{!isLoading ? (
-					pics.map(user => {
-						const { id, thumbnailUrl } = user;
-						return (
-							<div style={cell} key={id}>
-								<img value={id} onClick={(e) => this.handleClick(id, e)} src={thumbnailUrl}/>
-							</div>
-						);
-					})
-				// If there is a delay in data, let's let the user know it's loading
-				) : (
-					<h3>Loading...</h3>
-				)}
+
+		const renderPics = currentPics.map(user => {
+			const { id, thumbnailUrl } = user;
+			return (
+				<div style={cell} key={id}>
+					<img value={id} onClick={(e) => this.handleClick(id, e)} src={thumbnailUrl}/>
 				</div>
-		  	</React.Fragment>
-		);
+			);
+		});
+  
+		  // Logic for displaying page numbers
+		const pageNumbers = [];
+		  	for (let i = 1; i <= Math.ceil(pics.length / picsPerPage); i++) {
+				pageNumbers.push(i);
+		}
+
+		const renderPageNumbers = pageNumbers.map(number => {
+			return (
+				<li key={number} id={number} onClick={this.handlePage} style={pageStyle2}>{number}</li>
+			);
+		});
+
+		return (
+			<div>
+				<h3>Pictures</h3>
+				<ul id="page-numbers" style={pageStyle2}>
+					{renderPageNumbers}
+			  	</ul>
+				<p>Current Page: {this.state.currentPage}</p>
+			  	<div style={divStyle}>
+					{renderPics}
+			  	</div>
+			  	<ul id="page-numbers" style={pageStyle2}>
+					{renderPageNumbers}
+			  	</ul>
+				<p>Current Page: {this.state.currentPage}</p>
+			</div>
+		  );
 	}
 }
 
@@ -138,8 +182,8 @@ class Details extends React.Component {
 						return (
 							<div key={id}>
 								<button style={back} onClick={this.handleClick} >Back</button>
-								<img value={id} src={url}/>
 								<p>{title}</p>
+								<img value={id} src={url}/>
 							</div>
 						);
 					})
