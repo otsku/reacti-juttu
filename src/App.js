132927e1f0c11422bee90844f089ed986aef136e
otsku <otto.pelit@gmail.com>
2019-04-01 22:10:36 +0300

    viimeistelyä(kommentointia ja koodin siistimistä)

--- src/App.js
+++ src/App.js
@@ -12,18 +12,18 @@ class App extends React.Component {
 			Details: false,
 			id: null,
 			currentPage: 1,
-          	picsPerPage: 100
+      picsPerPage: 100
 		}
 		this.handleClick = this.handleClick.bind(this);
 		this.handlePage = this.handlePage.bind(this);
 	}
 	
+	//fetches pictures when site is loaded
 	componentDidMount() {
-		if(this.state.id === null) {
-			this.fetchPics();
-		}	
+		this.fetchPics();
 	}
 
+	//handles page change
 	handlePage(event) {
         this.setState({
           currentPage: Number(event.target.id)
@@ -46,13 +46,14 @@ class App extends React.Component {
 			.catch(error => this.setState({ error, isLoading: false }));
 	}
 
+	//handles clicking of the picture
 	handleClick(pic) {
 		this.setState({id: pic});
 		this.setState({Details: true});
 	}
 
 	render() {
-		//const { isLoading, pics, error } = this.state;
+		//style variables
 		const divStyle = {
 			display: 'flex',
 			flexWrap: 'wrap',
@@ -63,11 +64,14 @@ class App extends React.Component {
 			flexWrap: 'wrap',
 			padding: '4px 4px 4px 4px',
 		};
-		const pageStyle2 = {
-			flexWrap: 'wrap',
+		const pageStyle = {
 			listStyle: 'none',
 			display: 'flex',
-			paddingRight: '4px',
+		};
+		const pageStyle2 = {
+			listStyle: 'none',
+			display: 'flex',
+			paddingRight: '6px',
 			color: 'blue',
 			userSelect: 'none',
 			cursor: 'pointer',
@@ -75,16 +79,17 @@ class App extends React.Component {
 			fontSize: '1.2em',
 		};
 		const { pics, currentPage, picsPerPage } = this.state;
-
         // Logic for displaying current todos
         const indexOfLastPic = currentPage * picsPerPage;
         const indexOfFirstPic = indexOfLastPic - picsPerPage;
         const currentPics = pics.slice(indexOfFirstPic, indexOfLastPic);
 
+		//if picture has been clicked, goes to the details page
 		if(this.state.Details === true) {
 			return(<Details pic={this.state.id} />);
 		}
 
+		//Renders page pictures
 		const renderPics = currentPics.map(user => {
 			const { id, thumbnailUrl } = user;
 			return (
@@ -94,29 +99,31 @@ class App extends React.Component {
 			);
 		});
   
-		  // Logic for displaying page numbers
+		// Logic for displaying page numbers
 		const pageNumbers = [];
 		  	for (let i = 1; i <= Math.ceil(pics.length / picsPerPage); i++) {
 				pageNumbers.push(i);
 		}
 
+		//Renders page numbers
 		const renderPageNumbers = pageNumbers.map(number => {
 			return (
 				<li key={number} id={number} onClick={this.handlePage} style={pageStyle2}>{number}</li>
 			);
 		});
 
+		//renders the page(contains pictures and page numbers rendering)
 		return (
 			<div>
 				<h3>Pictures</h3>
-				<ul id="page-numbers" style={pageStyle2}>
+				<ul id="page-numbers"  style={pageStyle}>
 					{renderPageNumbers}
 			  	</ul>
 				<p>Current Page: {this.state.currentPage}</p>
 			  	<div style={divStyle}>
 					{renderPics}
 			  	</div>
-			  	<ul id="page-numbers" style={pageStyle2}>
+			  	<ul id="page-numbers" style={pageStyle}>
 					{renderPageNumbers}
 			  	</ul>
 				<p>Current Page: {this.state.currentPage}</p>
@@ -125,6 +132,7 @@ class App extends React.Component {
 	}
 }
 
+//Details Component shows chosen picture and title
 class Details extends React.Component {
 	constructor(props) {
 		super(props);
@@ -137,6 +145,7 @@ class Details extends React.Component {
 		this.handleClick = this.handleClick.bind(this);
 	}
 
+	//fetches the chosen picture
 	componentDidMount() {
 		this.fetchPics();
 	}
@@ -159,18 +168,20 @@ class Details extends React.Component {
 			.catch(errors => this.setState({ errors, Loading: false }));
 	}
 
+	//handles the clicking of back button
 	handleClick() {
 		this.setState({Details: false});
 	}
 
 	render() {
 		const { Loading, pic, errors } = this.state;
-		const back = {
-			top: '0px',
-		};
+
+		//if back is clicked returns to the first page
 		if(this.state.Details === false) {
 			return(<App/>);
 		}
+
+		//renders the details page
 		return (
 		 	<React.Fragment>
 				<h1>Pictures</h1>
@@ -181,8 +192,8 @@ class Details extends React.Component {
 						const { id, url, title } = user;
 						return (
 							<div key={id}>
-								<button style={back} onClick={this.handleClick} >Back</button>
-								<p>{title}</p>
+								<button onClick={this.handleClick} >Back</button>
+								<p>Title : {title}</p>
 								<img value={id} src={url}/>
 							</div>
 						);
